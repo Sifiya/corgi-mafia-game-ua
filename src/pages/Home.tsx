7c484b9889144bc7b1285304636a8530ba973827
Tetyana Hrynko <sofiyasuetina@gmail.com>
2025-02-26 13:56:47 +0200

    chore(shadcn): add shadcn components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,16 @@
 import { useTranslationContext } from '@/lib/i18n/context';
+import { Button } from '@/components/ui/button';
 import type { Component } from 'solid-js';
 
 const Home: Component = () => {
   const i18n = useTranslationContext();
   return (
-    <section class="grow w-full h-full flex items-center justify-center">
+    <section class="flex flex-col gap-4">
       <p>{i18n.t('MAIN_PAGE_PRE_TITLE')}</p>
       <h1>{i18n.t('MAIN_PAGE_TITLE')}</h1>
       <p>{i18n.t('MAIN_PAGE_DESCRIPTION')}</p>
-      <button>{i18n.t('MAIN_PAGE_BUTTON_START')}</button>
-      <button>{i18n.t('MAIN_PAGE_BUTTON_ADD_CORGI')}</button>
+      <Button variant="secondary">{i18n.t('MAIN_PAGE_BUTTON_ADD_CORGI')}</Button>
+      <Button>{i18n.t('MAIN_PAGE_BUTTON_START')}</Button>
     </section>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
-import path from 'path';
+import path from 'node:path';
 
 export default defineConfig({
   resolve: {
